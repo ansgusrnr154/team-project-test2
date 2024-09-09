6c829e2b036eed908c8457e07bb60980bcb00e0b
EC2 Default User <ec2-user@ip-172-31-46-145.ap-northeast-2.compute.internal>
2024-09-09 08:58:56 +0000

    test-version2

--- backend/src/main/java/com/egemmerce/hc/config/WebConfig.java
+++ backend/src/main/java/com/egemmerce/hc/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:8080")  // 리액트 개발 서버 주소
+                .allowedOrigins("http://www.mhk154.shop:8080")  // 리액트 개발 서버 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메소드들
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 인증 정보 허용
